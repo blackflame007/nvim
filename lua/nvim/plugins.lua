c4fecc239237458ca463672c9525cc176ef576f3
Nick L <lesseosnick@gmail.com>
2021-12-05 00:33:15 -0800

    Added: tab autodetct plugin

--- lua/nvim/plugins.lua
+++ lua/nvim/plugins.lua
@@ -1,71 +1,47 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    packer_bootstrap = fn.system({
+        'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path
+    })
 end
 
 return require('packer').startup(function(use)
-  -- My plugins here
-  use 'wbthomason/packer.nvim'
-  use 'glepnir/dashboard-nvim'
-  -- Status Line and Bufferline
-  use {
-  'romgrk/barbar.nvim',
-  requires = {'kyazdani42/nvim-web-devicons'}
-  }
-  use {
-  'nvim-lualine/lualine.nvim',
-  requires = {'kyazdani42/nvim-web-devicons', opt = true}
-  }
-  -- lsp
-  use {
-    'neovim/nvim-lspconfig',
-    'williamboman/nvim-lsp-installer',
-  }
+    -- My plugins here
+    use 'wbthomason/packer.nvim'
+    use 'glepnir/dashboard-nvim'
+    -- Status Line and Bufferline
+    use {'romgrk/barbar.nvim', requires = {'kyazdani42/nvim-web-devicons'}}
+    use {'nvim-lualine/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true}}
+    -- lsp
+    use {'neovim/nvim-lspconfig', 'williamboman/nvim-lsp-installer'}
 
-  use {
-      'L3MON4D3/LuaSnip',
-      'rafamadriz/friendly-snippets',
-      'hrsh7th/cmp-nvim-lsp',
-      'saadparwaiz1/cmp_luasnip',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-nvim-lua',
-      'hrsh7th/cmp-cmdline',
-      'hrsh7th/cmp-vsnip',
-      'hrsh7th/vim-vsnip',
-      'onsails/lspkind-nvim'
+    use {
+        'L3MON4D3/LuaSnip', 'rafamadriz/friendly-snippets', 'hrsh7th/cmp-nvim-lsp',
+        'saadparwaiz1/cmp_luasnip', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-nvim-lua',
+        'hrsh7th/cmp-cmdline', 'hrsh7th/cmp-vsnip', 'hrsh7th/vim-vsnip', 'onsails/lspkind-nvim'
     }
 
-    use 'hrsh7th/nvim-cmp' --completion
-    use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
+    use 'hrsh7th/nvim-cmp' -- completion
+    use {'tzachar/cmp-tabnine', run = './install.sh', requires = 'hrsh7th/nvim-cmp'}
     use 'simrat39/symbols-outline.nvim'
--- Telescope
-  use {
-   'nvim-telescope/telescope.nvim',
-   requires = { 
-	   	{'nvim-lua/plenary.nvim'},
-		{'nvim-lua/popup.nvim'}
-	   }
-  }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    -- Telescope
+    use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/plenary.nvim'}, {'nvim-lua/popup.nvim'}}}
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
     -- Treesitter
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use {
-    'numToStr/Comment.nvim',
-    requires = { {'JoosepAlviste/nvim-ts-context-commentstring'} },
-    config = function()
-        require('Comment').setup()
-    end
-  } 
-  -- Autopairs
-  use 'windwp/nvim-autopairs'
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    use {
+        'numToStr/Comment.nvim',
+        requires = {{'JoosepAlviste/nvim-ts-context-commentstring'}},
+        config = function()
+            require('Comment').setup()
+        end
+    }
+    -- Autopairs
+    use 'windwp/nvim-autopairs'
+    use 'tpope/vim-sleuth'
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then require('packer').sync() end
 end)
-
 
