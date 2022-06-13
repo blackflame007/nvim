2ddc15275694b46bc97af6ac27791b78232b696c
Nick L <lesseosnick@gmail.com>
2022-06-13 01:26:04 -0700

    Added: debugger config, treesitter conf, util funcs

--- lua/nvim/treesitter.lua
+++ lua/nvim/treesitter.lua
@@ -1,66 +1,33 @@
-require'nvim-treesitter.configs'.setup {
-    on_config_done = nil,
-    ensure_installed = {}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    ignore_install = {},
-    matchup = {
-        enable = false -- mandatory, false will disable the whole extension
-        -- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
-    },
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then return end
+
+local ft_to_parser = require"nvim-treesitter.parsers".filetype_to_parsername
+ft_to_parser.motoko = "typescript"
+
+configs.setup({
+    ensure_installed = "all", -- one of "all" or a list of languages
+    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+    ignore_install = {""}, -- List of parsers to ignore installing
     highlight = {
+        -- use_languagetree = true,
         enable = true, -- false will disable the whole extension
-        additional_vim_regex_highlighting = true,
-        disable = {"latex"}
+        -- disable = { "css", "html" }, -- list of language that will be disabled
+        disable = {"css", "markdown"}, -- list of language that will be disabled
+        additional_vim_regex_highlighting = true
     },
-    context_commentstring = {
-        enable = true,
-        config = {
-            -- Languages that have a single comment style
-            typescript = "// %s",
-            css = "/* %s */",
-            scss = "/* %s */",
-            html = "<!-- %s -->",
-            svelte = "<!-- %s -->",
-            vue = "<!-- %s -->",
-            json = ""
-        }
-    },
-    -- indent = {enable = true, disable = {"python", "html", "javascript"}},
-    -- TODO seems to be broken
-    indent = {enable = true, disable = {"yaml"}},
-    autotag = {enable = false},
-    textobjects = {
-        swap = {
-            enable = false
-            -- swap_next = textobj_swap_keymaps,
-        },
-        -- move = textobj_move_keymaps,
-        select = {
-            enable = false
-            -- keymaps = textobj_sel_keymaps,
-        }
-    },
-    textsubjects = {enable = false, keymaps = {["."] = "textsubjects-smart", [";"] = "textsubjects-big"}},
-    playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = false, -- Whether the query persists across vim sessions
-        keybindings = {
-            toggle_query_editor = "o",
-            toggle_hl_groups = "i",
-            toggle_injected_languages = "t",
-            toggle_anonymous_nodes = "a",
-            toggle_language_display = "I",
-            focus_language = "f",
-            unfocus_language = "F",
-            update = "R",
-            goto_node = "<cr>",
-            show_help = "?"
-        }
-    },
+    autopairs = {enable = true},
+    indent = {enable = true, disable = {"python", "css"}},
+    context_commentstring = {enable = true, enable_autocmd = false},
+    autotag = {enable = true, disable = {"xml"}},
     rainbow = {
-        enable = false,
-        extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-        max_file_lines = 1000 -- Do not enable for files with more than 1000 lines, int
-    }
-}
+        enable = true,
+        colors = {
+            "Gold", "Orchid", "DodgerBlue"
+            -- "Cornsilk",
+            -- "Salmon",
+            -- "LawnGreen",
+        },
+        disable = {"html"}
+    },
+    playground = {enable = true}
+})
