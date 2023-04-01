c5873b690f36d7f9f4546786cb7a575e050ac677
Nick L <lesseosnick@gmail.com>
2023-03-31 18:59:49 -0700

    Updated: dressing conf

--- lua/nvim/dressing.lua
+++ lua/nvim/dressing.lua
@@ -30,11 +30,12 @@ dressing.setup {
     -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
     max_width = { 140, 0.9 },
     min_width = { 20, 0.2 },
-
-    -- Window transparency (0-100)
-    winblend = 10,
-    -- Change default highlight groups (see :help winhl)
-    winhighlight = "",
+    win_options = {
+      -- Window transparency (0-100)
+      winblend = 10,
+      -- Change default highlight groups (see :help winhl)
+      winhighlight = "",
+    },
 
     override = function(conf)
       -- This is the config that will be passed to nvim_open_win.
@@ -90,11 +91,13 @@ dressing.setup {
       -- 'editor' and 'win' will default to being centered
       relative = "editor",
 
+
+    win_options = {
       -- Window transparency (0-100)
       winblend = 10,
       -- Change default highlight groups (see :help winhl)
       winhighlight = "",
-
+    },
       -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
       -- the min_ and max_ options can be a list of mixed types.
       -- max_width = {140, 0.8} means "the lesser of 140 columns or 80% of total"
