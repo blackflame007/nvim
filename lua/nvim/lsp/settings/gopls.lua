0313664026a2a2db437a817787ce1d2c5dfbb4ad
Nick L <lesseosnick@gmail.com>
2023-05-18 14:31:49 -0700

    Added: cmp back

--- lua/nvim/lsp/settings/gopls.lua
+++ lua/nvim/lsp/settings/gopls.lua
@@ -1,18 +1,18 @@
 local util = require "lspconfig/util"
 
 return {
-    -- on_attach = on_attach,
-    -- capabilities = capabilities,
-    cmd = {"gopls"},
-    filetypes = { "go", "gomod", "gowork", "gotmpl" },
-    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-    settings = {
-      gopls = {
-        completeUnimported = true,
-        usePlaceholders = true,
-        analyses = {
-          unusedparams = true,
-        },
+  on_attach = require("nvim.lsp.handlers").on_attach,
+  capabilities = require("nvim.lsp.handlers").capabilities,
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
       },
     },
-  }
+  },
+}
