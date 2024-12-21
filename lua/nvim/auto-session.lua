786fd59379d7cb3d80c861a6c35784e4d4f89910
Nick L <lesseosnick@gmail.com>
2024-12-21 12:34:43 -0800

    Updated: autosession config

--- lua/nvim/auto-session.lua
+++ lua/nvim/auto-session.lua
@@ -14,17 +14,13 @@ if not l_status_ok then
 end
 
 local opts = {
-  log_level = "info",
+  log_level = "error",
   auto_session_enable_last_session = false,
   auto_session_root_dir = vim.fn.stdpath "data" .. "/sessions/",
   auto_session_enabled = true,
-  auto_save_enabled = nil,
-  auto_restore_enabled = nil,
-  auto_session_suppress_dirs = {
-    -- vim.fn.glob(vim.fn.stdpath "config" .. "/*"),
-    os.getenv "HOME",
-    -- os.getenv "HOME" .. "/Machfiles",
-  },
+  auto_save_enabled = false,
+  auto_restore_enabled = false,
+  auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
   auto_session_use_git_branch = nil,
   -- the configs below are lua only
   bypass_session_save_file_types = { "alpha" },
